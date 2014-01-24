5092849d4fa72d728e577437a2ed3c4d87240b88
Kirsten Van Langenhoven <kvanlang@ucsd.edu>
2014-01-24 03:39:15 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -13,29 +13,37 @@ function initializePage() {
 		$('.jumbotron h1').text("Javascript is connected");
 		$("#testjs").text("Please wait...");
 		$(".jumbotron p").toggleClass("active");
-
 		$("a.thumbnail").click(projectClick);
+		$("#submitBtn").click(updateProject);
 	});
+}
 
 function projectClick(e) {
 	e.preventDefault();
 
-	$(this).css("background-color", "#7fff00")
+	$(this).css("background-color", "#7fff00");
 
 	var containingProject = $(this).closest(".project");
-	var description = $(containingProject).find(".project-description");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } 
+    else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 
-	if (description.length == 0) {
-		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
-	} else {
-		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-	}
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    $("description").fadeOut();
+}
 
-    $(".project-description").fadeOut();
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
 
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
 }
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
